8a2e6546b074c5a536efb510d5dd1e4a6abe0ad0
kr1viah_ <kravpolet@gmail.com>
2025-09-30 20:23:19 +0200

    More precise PoF

--- src/client/java/kr1v/dataCollector/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/client/java/kr1v/dataCollector/mixin/ClientPlayNetworkHandlerMixin.java
@@ -2,12 +2,16 @@ package kr1v.dataCollector.mixin;
 
 import kr1v.dataCollector.DataCollectorClient;
 import kr1v.dataCollector.GameMode;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.FabricUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,6 +25,7 @@ public class ClientPlayNetworkHandlerMixin {
 
 	@Inject(method = "onScreenHandlerSlotUpdate", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V"))
 	private void injected(ScreenHandlerSlotUpdateS2CPacket packet, CallbackInfo ci) {
+
 		if (DataCollectorClient.currentGame != GameMode.CCG_PILLARS_OF_FORTUNE) return;
 		if (MinecraftClient.getInstance().player == null) return;
 		final int count = packet.getStack().getCount();
@@ -33,8 +38,25 @@ public class ClientPlayNetworkHandlerMixin {
 		final boolean bl4 = previousPacket.getSlot() == packet.getSlot();
 		final boolean bl5 = (packet.getStack().get(DataComponentTypes.DAMAGE) == null) || packet.getStack().get(DataComponentTypes.DAMAGE) == 0;
 
+		final String[] t = {""};
 		if (bl1 && bl2 && bl3 && bl4 && bl5) {
+			packet.getStack().getComponents().forEach((a) -> {
+				t[0] += a.toString();
+				if (FabricLoader.getInstance().isDevelopmentEnvironment()) System.out.println(a);
+			});
+		}
+
+		final boolean bl6 = t[0].contains("repair_cost") &&
+			t[0].contains("max_stack_size") &&
+			t[0].contains("lore") &&
+			t[0].contains("break_sound") &&
+			t[0].contains("tooltip_display") &&
+			t[0].contains("attribute_modifiers") &&
+			t[0].contains("enchantments");
+
+		if (bl1 && bl2 && bl3 && bl4 && bl5 && bl6) {
 			String item = packet.getStack().getItem().toString().replace("minecraft:", "");
+			if (FabricLoader.getInstance().isDevelopmentEnvironment()) MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.literal(item).formatted(Formatting.BLUE));
 			DataCollectorClient.data.listOfPoFGames.getLast().items.add(item);
 		}
 		previousPacket = packet;
